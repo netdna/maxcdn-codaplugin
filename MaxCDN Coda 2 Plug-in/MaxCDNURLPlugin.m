36b22bb106eed2600a657f0de8bc04cf4332df52
Joe Dakroub <jdakroub@bonddesk.com>
2013-04-19 14:23:07 -0400

    Added CodaPlugInMinimumAPIVersion to Info.plist and cleaned up matching logic. Still need to work on replacing each match with the URL.

--- MaxCDN Coda 2 Plug-in/MaxCDNURLPlugin.m
+++ MaxCDN Coda 2 Plug-in/MaxCDNURLPlugin.m
@@ -8,7 +8,7 @@
 
 NSString * const kDefaultsURLKey = @"MaxCDNURL";
 NSString * const kExtensionJoinString = @"|";
-NSString * const kProtocolRegex = @"(ftp|http(s))";
+NSString * const kProtocolRegex = @"(://)";
 NSString * const kSheetXIB = @"URLSheet";
 NSString * const kURLRegex = @"([^\\s|\"|'|=|(]+.(%@))";
 
@@ -37,13 +37,13 @@ NSString * const kURLRegex = @"([^\\s|\"|'|=|(]+.(%@))";
 #pragma -
 #pragma CodaPlugiInsController Callbacks
 
-//2.0 and lower
+// Coda 2.0 and lower
 - (id)initWithPlugInController:(CodaPlugInsController*)aController bundle:(NSBundle*)aBundle
 {
     return [self initWithController:aController];
 }
 
-//2.0.1 and higher
+// Coda 2.0.1 and higher
 - (id)initWithPlugInController:(CodaPlugInsController*)aController plugInBundle:(NSObject <CodaPlugInBundle> *)plugInBundle
 {
     return [self initWithController:aController];
@@ -152,51 +152,48 @@ NSString * const kURLRegex = @"([^\\s|\"|'|=|(]+.(%@))";
     if ([defaults objectForKey:kDefaultsURLKey] != nil)
         [_URLTextField setStringValue:[defaults valueForKey:kDefaultsURLKey]];
 
-    [NSApp beginSheet:_sheet
-       modalForWindow:[textView window]
-        modalDelegate:self
-       didEndSelector:nil
-          contextInfo:nil];
+    [NSApp beginSheet:_sheet modalForWindow:[textView window] modalDelegate:self didEndSelector:nil contextInfo:nil];
 }
 
 - (void)insertURL:(id)sender
 {
-    NSString *extensions = [supportedFileExtensions componentsJoinedByString:kExtensionJoinString];
-    NSString *regex = [NSString stringWithFormat:kURLRegex, extensions];
-    NSMutableString *mutableString = [NSMutableString stringWithString:[textView string]];
+    [textView beginUndoGrouping];
     
-    [mutableString enumerateStringsMatchedByRegex:regex usingBlock:^(NSInteger captureCount,
-                                                                     NSString * const capturedStrings[captureCount],
-                                                                     const NSRange capturedRanges[captureCount],
+    NSString *joinedSupportedFileExtensions = [supportedFileExtensions componentsJoinedByString:kExtensionJoinString];
+    NSString *regex = [NSString stringWithFormat:kURLRegex, joinedSupportedFileExtensions];
+    NSMutableString *textViewString = [NSMutableString stringWithString:[textView string]];
+    
+    [textViewString enumerateStringsMatchedByRegex:regex usingBlock:^(NSInteger captureCount,
+                                                                     NSString *const __unsafe_unretained *capturedStrings,
+                                                                     const NSRange *capturedRanges,
                                                                      volatile BOOL *const stop)
-     {
-         NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-         NSString *CDNURL = [defaults objectForKey:kDefaultsURLKey];
-         
-         if ([[capturedStrings[0] componentsMatchedByRegex:kProtocolRegex] count] == 0)
-         {
-             
-             NSString *URLPrefix = [CDNURL hasSuffix:@"/"] ? CDNURL : [CDNURL stringByAppendingString:@"/"];
-             NSString *updatedMatch = [capturedStrings[0] hasPrefix:@"/"] ? [capturedStrings[0] substringFromIndex:1] : capturedStrings[0];
-             NSString *newURL = [NSString stringWithFormat:@"%@%@", URLPrefix, updatedMatch];
-             
-             [textView replaceCharactersInRange:capturedRanges[0] withString:newURL];
-         }
-     }];
+    {
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        NSString *CDNURL = [defaults objectForKey:kDefaultsURLKey];
+        
+        // Only convert URLs that do not have a protocol in front of them
+        if ([[capturedStrings[0] componentsMatchedByRegex:kProtocolRegex] count] == 0)
+        {
+            // Append a / to end of URLs that do no have one
+            NSString *URLPrefix = [CDNURL hasSuffix:@"/"] ? CDNURL : [CDNURL stringByAppendingString:@"/"];
+            
+            // Normalize the front of the matched URL
+            NSString *updatedMatch = [capturedStrings[0] hasPrefix:@"/"] ? [capturedStrings[0] substringFromIndex:1] : capturedStrings[0];
+            
+            // Append CDN URL to matched URL
+            NSString *newURL = [NSString stringWithFormat:@"%@%@", URLPrefix, updatedMatch];
+
+//            [textView replaceCharactersInRange:capturedRanges[0] withString:newURL];
+            [textView insertText:newURL];
+            [textView insertText:@"\n"];
+        }
+    }];
+    
+    [textView endUndoGrouping];
 }
 
 #pragma -
 #pragma Helpers
-
-- (BOOL)isSupportedFileExtension
-{
-    textView = [controller focusedTextView:self];
-    
-    if ([textView path])
-        return [supportedFileExtensions containsObject:[[textView path] pathExtension]];
-    
-    return NO;
-}
 
 - (NSString *)localizedStringForKey:(NSString *)key
 {
